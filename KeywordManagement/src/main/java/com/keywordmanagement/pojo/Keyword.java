ee4b225960f8dda461d86ab2118df194fd06189c
aotusoft <1520351630@qq.com>
2023-07-04 09:01:08 +0800

    page offset and Dynamic SQL

---
+++ KeywordManagement/src/main/java/com/keywordmanagement/pojo/Keyword.java
@@ -0,0 +1,47 @@
+package com.keywordmanagement.pojo;
+
+
+public class Keyword {
+
+  private long id;
+  private String name;
+  private String type;
+  private java.sql.Date createDate;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  public java.sql.Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(java.sql.Date createDate) {
+    this.createDate = createDate;
+  }
+
+}
